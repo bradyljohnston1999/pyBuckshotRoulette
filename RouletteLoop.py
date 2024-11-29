4b3dfe8c2bd8237fe1b48713b18414231678591e
bradyljohnston1999 <69018084+bradyljohnston1999@users.noreply.github.com>
2024-11-29 17:43:27 -0600

    Updated loop

--- RouletteLoop.py
+++ RouletteLoop.py
@@ -1,10 +1,14 @@
+from RouletteUtilz import clean, Colors
+
+
 class BuckshotRoulette:
     def __init__(self):
         self.is_running = False
 
     def run_loop(self):
+        clean()
         self.is_running = not self.is_running
         while self.is_running:
-            print("BuckshotRoulette loop")
-            input("Press Enter to continue...")
+            print(f"{Colors.YELLOW}{Colors.UNDERLINE}BuckshotRoulette{Colors.RESET} {Colors.GREEN}{Colors.BLINK}loop", Colors.RESET)
+            input("\nPress Enter to continue...")
             self.is_running = False
--- RouletteUtilz.py
+++ RouletteUtilz.py
@@ -1,2 +1,16 @@
-class RouletteUtilz:
-    pass
+import os
+
+
+def clean():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+class Colors:
+    WHITE = '\033[97m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    BLUE = '\033[94m'
+    YELLOW = '\033[93m'
+    UNDERLINE = '\033[4m'
+    BLINK = '\033[5m'
+    RESET = '\033[0m'
